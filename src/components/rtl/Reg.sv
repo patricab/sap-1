62d615c1328ec88e91e77f80a7453e2f83e4155f
patricab <patric.berthelsen@gmail.com>
2023-03-19 10:20:02 +0100

    feat: functional sim for register

---
+++ src/components/rtl/Reg.sv
@@ -0,0 +1,37 @@
+module Reg #(
+    parameter WIDTH = 8
+) (
+    input logic clk, rst,
+    input logic load_n, enable_n,
+    input logic [WIDTH-1:0] reg_in,
+    output logic [WIDTH-1:0] reg_out
+);
+
+reg [7:0] internal;
+    
+always @(posedge clk) 
+begin 
+    // Load signal
+    if (rst) begin
+       internal <= 8'b0;
+    end else begin
+        if (load_n == 1'b0 ) begin
+            internal <= reg_in;
+        end
+    end
+
+    reg_out <= internal; 
+    // if (enable_n == 1'b0) begin
+    //     reg_out <= internal; 
+    // end else begin
+    //     reg_out <= reg_out; 
+    // end
+end
+
+// Dump waves
+initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1, Reg);
+end
+
+endmodule
--- src/components/sim/test_reg.py
+++ src/components/sim/test_reg.py
@@ -1,28 +1,30 @@
 import random
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import FallingEdge
+from cocotb.triggers import RisingEdge
+from cocotb.triggers import Timer
+from cocotb.binary import BinaryValue
 
 @cocotb.test()
 async def test_reg(dut):
-    """ Test that d propagates to q """
-
     clock = Clock(dut.clk, 10, units="us")  # Create a 10us period clock on port clk
     cocotb.start_soon(clock.start())  # Start the clock
+    reg_check = 255
 
-    # for i in range(10):
-    # val = random.randint(0, 1)
-    # dut.d.value = val  # Assign the random value val to the input port d
-    reg = [1, 0, 1, 0, 1, 0, 1, 0]
-    dut.rst.value = 1
-    dut.enable_n.value = 1
-    dut.load_n.value = 0
-    dut.rst.value = 0
+    dut.enable_n = 1
+    dut.load_n = 1
+    dut.rst = 1
+    await Timer(10, units="us")
+    dut.rst = 0
+    await Timer(10, units="us")
+    dut.load_n = 0
+    await Timer(10, units="us")
 
-    dut.reg_in.value = reg
-
-    dut.load_n.value = 1
-    dut.enable_n.value = 0
-    # await RisingEdge(dut.clk)
-    await Timer(250, units="ns")
-    assert dut.reg_out.value == reg, "output was incorrect on the {}th cycle"
+    dut.reg_in = reg_check
+    await Timer(10, units="us")
+    dut.load_n = 1
+    await Timer(20, units="us")
+    dut.enable_n = 0
+    await RisingEdge(dut.clk)
+    print(dut.reg_out.value.binstr)
+    assert dut.reg_out.value == reg_check, "No output match!"
